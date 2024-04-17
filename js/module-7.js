9835c0dcf6e0edc9a68fa40621036520d3012979
Asia <royfinaa@gmail.com>
2024-04-17 12:40:03 +0300

    update

---
+++ js/module-7.js
@@ -0,0 +1,10 @@
+// const link = document.querySelector(".link");
+// console.log(link.href);
+// link.href = "http://neo.goit.global";
+// console.log(link.href);
+
+const el = document.querySelector(".text")
+const nested = document.querySelector(".sub-text")
+
+console.log(el.textContent);
+el.textContent = "Username: Poly";
